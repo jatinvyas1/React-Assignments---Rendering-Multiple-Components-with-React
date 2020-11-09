aba562e0746741231bb00102d0291b74e5a4150c
jatinvyas1 <48468154+jatinvyas1@users.noreply.github.com>
2020-11-09 20:35:44 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
   return (
-    <></>
-  )
-}
-
+    <>
+      <p data-ns-test="project-name">Name</p>
+      <p data-ns-test="project-description">Description</p>
+    </>
+  );
+};
 
 export default App;
